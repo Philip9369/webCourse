c8670fb55ba20a58706b85b82ca8e423e1a16be0
Philip9369 <philip9369@gmail.com>
2025-06-07 22:05:03 -0400

    Second commit

---
+++ yelpcamp/routes/campgrounds.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+const Campground = require("../models/campground");
+const Review = require("../models/review");
+
+router.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+router.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+router.post(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError("Invalid Data", 400);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
+    console.log(campground);
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+router.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+router.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Campground.findByIdAndDelete(id, req.body);
+  res.redirect("/campgrounds");
+  // res.send("easter")
+});
+
+module.exports = router;
