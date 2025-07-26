eef60cee2b1d516037a40f9ab49694e6cd30a993
Philip9369 <philip9369@gmail.com>
2025-07-26 19:40:01 -0400

    Eighth Commit

---
+++ bCryptDemo/index.js
@@ -0,0 +1,79 @@
+const bcyrpt = require("bcrypt");
+const express = require("express");
+const app = express();
+const User = require("./models/user");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+mongoose
+  .connect("mongodb://localhost:27017/authDemo", {
+    // All Three are Deprecated functions
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Mongo Connect");
+  })
+  .catch((err) => {
+    console.log("Mongo Fail");
+    console.log(err);
+  });
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "sauce" }));
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcyrpt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  req.session.user_id = user._id;
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const { password, username } = req.body;
+  const user = await User.findOne({ username });
+  const validPassword = await bcyrpt.compare(password, user.password);
+  if (validPassword) {
+    req.session.user_id = user._id;
+    res.send("Yayyyyy");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/logout", (req, res) => {
+  req.session.user_id = null;
+  res.redirect("/login");
+});
+
+app.get("/secret", (req, res) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  res.render("secret");
+});
+
+app.listen("3000", () => {
+  console.log("3000 Port Active");
+});
