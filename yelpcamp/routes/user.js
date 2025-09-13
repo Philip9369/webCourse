16aa6a1a15f3bf66a88090d7ca4bcf5aec3f6975
Philip9369 <philip9369@gmail.com>
2025-09-13 14:42:30 -0400

    Eleventh Commit

--- yelpcamp/routes/user.js
+++ yelpcamp/routes/user.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 router.post("/register", async (req, res) => {
-  res.send(req.body);
+  const { email, username, password } = req.body;
+  const user = new User({ email, username });
+  const registerUser = await User.register(user, password);
+  console.log(registerUser);
+  req.flash("success", "Welcome to Yelp Camp!");
+  res.redirect("/campgrounds");
 });
 
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome Back!");
+    res.redirect("/campgrounds");
+  }
+);
 module.exports = router;
